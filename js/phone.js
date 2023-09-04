0eead413dd841e07aaab652dc4540469490c4a1b
Yousufjoy <kanekiken333@gmail.com>
2023-09-04 11:31:36 +0600

    final commit

--- js/phone.js
+++ js/phone.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText = "Iphone") => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   ); // fetch koro kintu ektu wait koro, data load howar jonno  tay await
@@ -12,7 +12,17 @@ const displayPhones = (phones) => {
   //1) Where to add
   const phoneContainer = document.getElementById("phone-container");
   // clear phone container  cards before adding new cards
-  phoneContainer.textContent = "";
+
+  phoneContainer.textContent = ""; // search history ager ta ar thake na
+
+  // Phone length 12 er besi hole ekta button dekhabe
+  if (phones.length > 12) {
+    const showAll = document.getElementById("show-all");
+    showAll.classList.remove("hidden");
+  } else {
+    showAll.classList.add("hidden");
+  }
+  phones = phones.slice(0, 5); // phone k set kore ditesi j only 5ta phone show korbe webite e
   phones.forEach((phone) => {
     // 2) What to add create a div
     const phoneCard = document.createElement("div");
@@ -29,18 +39,62 @@ const displayPhones = (phones) => {
               <h2 class="card-title">${phone.brand}</h2>
               <p>${phone.phone_name}</p>
               <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+                <button onClick="handleDetails('${phone.slug}')" class="btn btn-primary">Details</button>
               </div>
             </div>
        `; //4) AppendChild
     phoneContainer.appendChild(phoneCard);
   });
+  loadingSpinning(false);
 };
 
 //handleSearch
 
 const handleSearch = () => {
+  loadingSpinning(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   loadPhone(searchText);
 };
+
+const loadingSpinning = (isLoading) => {
+  const loadingScreen = document.getElementById("loading-screen");
+  if (isLoading) {
+    loadingScreen.classList.remove("hidden");
+  } else {
+    loadingScreen.classList.add("hidden");
+  }
+};
+
+// Handle Details
+
+const handleDetails = async (id) => {
+  // console.log("clicked", id);
+
+  // load single phone data
+
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  const data = await res.json();
+  const phone = data.data;
+  showPhoneDetails(phone);
+};
+
+const showPhoneDetails = (phone) => {
+  console.log(phone);
+  const phoneName = document.getElementById("show-phone-name");
+  phoneName.innerText = `Name : ${phone.name}`;
+
+  const showDtailContent = document.getElementById("show-detail-container");
+  showDtailContent.innerHTML = `
+  <img src=${phone.image} alt="" />
+  <p> Storage ${phone?.mainFeatures.storage}</p>
+  `;
+
+  // show the moodal
+
+  show_details_modal.showModal();
+};
+
+loadPhone();
